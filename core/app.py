d8ee5723f6272cffcfd3bb29914b84828d7ff219
Ivan Maslyuk <ivan.maslyuck@live.com>
2021-09-09 14:37:49 +0300

    Scale once

--- core/app.py
+++ core/app.py
@@ -12,20 +12,19 @@ class App:
         self.window_width = window_width
         self.window_height = window_height
         self.window_title = window_title
+        self._scaled = False
 
     def draw(self, window, surface):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
         with surface as canvas:
-            canvas.scale(*glfw.get_window_content_scale(window))
+            if not self._scaled:
+                canvas.scale(*glfw.get_window_content_scale(window))
+                self._scaled = True
             start_time = time.time()
             self.root_view.draw(canvas, 0, 0, self.window_width, self.window_height)
             print('Draw time:', round((time.time() - start_time) * 1000, 5), 'ms')
         surface.flushAndSubmit()
         glfw.swap_buffers(window)
-
-    # def window_size_callback(self, window, surface):
-    #     def func(window, width, height):
-    #         self.
 
     @contextlib.contextmanager
     def skia_surface(self, window):
@@ -64,10 +63,7 @@ class App:
             GL.glClearColor(255, 255, 255, 255)
 
             with self.skia_surface(window) as surface:
-                print(dir(surface))
                 def window_size_callback(_, width, height):
-                    width_scale, height_scale = glfw.get_window_content_scale(window)
-                    surface.width = int(self.window_width * width_scale)
                     self.window_width = width
                     self.window_height = height
                     self.draw(window, surface)
